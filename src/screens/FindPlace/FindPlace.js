92f2e432a6bc48c35be34138a4853d16ccca4317
Nikolay Buhay <mali3days@gmail.com>
2018-10-14 01:04:51 +0300

    :sparkles: change FindPlaceScreen & SharePlaceScreen screens

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,14 +1,25 @@
 import React, { PureComponent } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import PlaceList from '../../components/PlaceList/PlaceList';
 
 class FindPlaceScreen extends PureComponent {
     render() {
+        const { places } = this.props;
+
         return(
             <View>
-                <Text>FindPlaceScreen</Text>
+                <PlaceList places={places}/>
             </View>
         );
     }
 }
 
-export default FindPlaceScreen;
+const mapStateToProps = (state) => {
+    return {
+        places: state.places.places,
+    };
+};
+
+export default connect(mapStateToProps)(FindPlaceScreen);
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,14 +1,28 @@
 import React, { PureComponent } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import PlaceInput from '../../components/PlaceInput/PlaceInput';
+import { addPlace } from '../../store/actions/index';
 
 class SharePlaceScreen extends PureComponent {
+    placeAddedHandler = (placeName) => {
+        this.props.onAddPlace(placeName);
+    }
+
     render() {
         return(
             <View>
-                <Text>SharePlaceScreen</Text>
+                <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
             </View>
         );
     }
 }
 
-export default SharePlaceScreen;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPlace: (placeName) => dispatch(addPlace(placeName)),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(SharePlaceScreen);
