3ef4e66c55a40c59b0e3a87339b325bdf2b46258
Nikolay Buhay <mali3days@gmail.com>
2018-10-15 13:49:53 +0300

    :fire: move placeDetail file, remove modal, make it screen

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,38 +1,28 @@
 import React from 'react';
-import {Modal,View,Text,Image,Button,StyleSheet,TouchableOpacity} from 'react-native';
+import {View,Text,Image,Button,StyleSheet,TouchableOpacity} from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const placeDetail = ({place,onItemDelete,onModalClose}) => {
-    getModalContent = (place) => {
-        if (place === null) return;
-        return (
-            <View>
-                <Image source={place.image} style={styles.placeImage}/>
-                <Text style={styles.placeName}>{place.name}</Text>
-            </View>
-        );
-    };
-
+const placeDetail = ({place,onItemDelete}) => {
     return (
-        <Modal visible={place !== null} animationType='slide' onRequestClose={onModalClose}>
-            <View style={styles.modalContainer}>
-                {this.getModalContent(place)}
+            <View style={styles.сontainer}>
+                <View>
+                    <Image source={place.image} style={styles.placeImage}/>
+                    <Text style={styles.placeName}>{place.name}</Text>
+                </View>
                 <View>
                     <TouchableOpacity onPress={onItemDelete}>
                         <View style={styles.deleteButton}>
                             <Icon size={30} name='ios-trash' color='red'/>
                         </View>
                     </TouchableOpacity>
-                    <Button title='Close' onPress={onModalClose}/>
                 </View>
             </View>
-        </Modal>
     );
 };
 
 const styles = StyleSheet.create({
-    modalContainer: {
+    сontainer: {
         margin: 22,
     },
     placeImage: {
