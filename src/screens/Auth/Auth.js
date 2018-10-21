4f7b1c11fed8dfb86a02bb799673e5fafea1819a
Nikolay Buhay <mali3days@gmail.com>
2018-10-21 21:27:35 +0300

    :sparkles: add extra inputs to the login screen & change styles

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../startMainTabs';
 
@@ -10,12 +10,32 @@ class AuthScreen extends PureComponent {
 
     render() {
         return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title='Login' onPress={this.loigIn}/>
+            <View style={styles.container}>
+                <Text>Please Log In</Text>
+                <Button title="Switch to Login" onPress={this.loigIn}/>
+                <View style={styles.inputContainer}>
+                    <TextInput style={styles.input} placeholder="Email" />
+                    <TextInput style={styles.input}  placeholder="Password" />
+                    <TextInput style={styles.input}  placeholder="Confirm Password" />
+                </View>
+                <Button title='Submit' onPress={this.loigIn}/>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputContainer: {
+        width: '80%',
+    },
+    input: {
+        width: '100%',
+    }
+});
 
 export default AuthScreen;
